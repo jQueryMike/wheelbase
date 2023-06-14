33d4f318143bbf1bff755d8a8d85aa073c2ece55
Steve Deighton <spd@clickdealer.co.uk>
2023-06-14 14:04:02 +0100

    CLICKWEB-000: fixes typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 export const metadata = {
   title: 'Click Websites',
-  description: 'This is the app that will run Click Web./',
+  description: 'This is the app that will run Click Web.',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
