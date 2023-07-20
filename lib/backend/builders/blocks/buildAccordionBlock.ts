3e5f613b1777f7c573b26a027fd2bd6a8b2735c4
Steve Deighton <spd@clickdealer.co.uk>
2023-07-20 13:58:38 +0100

    CLICKWEB-332: add headings block to accordion

--- lib/backend/builders/blocks/buildAccordionBlock.ts
+++ lib/backend/builders/blocks/buildAccordionBlock.ts
@@ -6,7 +6,7 @@ import buildAdditionalContent from '../buildAdditionalContent';
 import buildTheme from '../buildTheme';
 import extractClassOverrides from '../extractClassOverrides';
 import buildHeadingBlock from './buildHeadingBlock';
-import buildSubheadingBlock from './buildSubheadingBlock';
+import buildHeadingsBlock from './buildHeadingsBlock';
 
 const buildAccordionBlock = ({
   id,
@@ -39,34 +39,18 @@ const buildAccordionBlock = ({
       instanceOverrides,
     });
 
-    // Add heading
-    const heading = content?.heading?.items[0];
-    if (heading) {
-      const headingThemeProperties = globalAccordionThemeProperties?.headingTheme?.items[0]?.content?.properties;
+    // Add headings
+    const headings = content?.headings?.items[0];
+    if (headings) {
+      const headingsThemeProperties = globalAccordionThemeProperties?.headingsTheme?.items[0]?.content?.properties;
 
-      accordion.heading = buildHeadingBlock({
-        id: heading.content.id,
-        name: 'Heading',
-        content: heading.content.properties,
-        settings: heading.settings.properties,
-        parentVariantId: headingThemeProperties?.themeVariant,
-        parentOverrides: extractClassOverrides(headingThemeProperties),
-        globalTheme,
-      });
-    }
-
-    // Add subheading
-    const subheading = content?.subheading?.items[0];
-    if (subheading) {
-      const subheadingThemeProperties = globalAccordionThemeProperties?.subheadingTheme?.items[0]?.content?.properties;
-
-      accordion.subheading = buildSubheadingBlock({
-        id: subheading.content.id,
-        name: 'Subheading',
-        content: subheading.content.properties,
-        settings: subheading.settings.properties,
-        parentVariantId: subheadingThemeProperties?.themeVariant,
-        parentOverrides: extractClassOverrides(subheadingThemeProperties),
+      accordion.headings = buildHeadingsBlock({
+        id: headings.content.id,
+        name: 'Headings',
+        content: headings.content.properties,
+        settings: headings.settings.properties,
+        parentVariantId: headingsThemeProperties?.themeVariant,
+        parentOverrides: extractClassOverrides(headingsThemeProperties),
         globalTheme,
       });
     }
@@ -108,8 +92,8 @@ const buildAccordionBlock = ({
           });
         }
 
-        accordionItem.content = buildAdditionalContent({
-          items: itemContent.content?.items,
+        accordionItem.contentArea = buildAdditionalContent({
+          items: itemContent.contentArea?.items,
           parentThemeProperties: globalAccordionThemeProperties,
           globalTheme,
         });
