74ab9462028c4aaefdfca678305802d91e8563ca
ClickJWalton <james.walton@clickdealer.co.uk>
2023-09-12 09:14:33 +0100

    CLICKWEB-349b: add depth to nav storybook

--- lib/components/blocks/DrawerNavigation/stories/DrawerNavigation.stories.tsx
+++ lib/components/blocks/DrawerNavigation/stories/DrawerNavigation.stories.tsx
@@ -10,13 +10,20 @@ export const Variant1: Story = {
     classes: drawerNavigationVariant1.classes,
     menuButtonIcon: 'fa-solid fa-bars',
     closeButtonIcon: 'fa-solid fa-xmark',
-    toggleButtonIcon: 'fa-brands fa-twitter',
+    toggleButtonIcon: 'fa-solid fa-arrow-down',
     items: [
       {
         id: '6b57afa5-a42b-4bc3-bc88-d4afddfbd97f',
         href: '/',
         text: 'Home',
-        children: [],
+        children: [
+          {
+            id: '5b52ara5-a43a-43c3-ba38-f46sgrnhj597p',
+            href: '/contact-us',
+            text: 'Contact Us',
+            children: [],
+          },
+        ],
       },
     ],
   },
