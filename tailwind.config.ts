fe43d892b3166702e9c6798e5b81b7b94d7a4bab
Steve Deighton <spd@clickdealer.co.uk>
2023-10-09 16:42:35 +0100

    CLICKWEB-000: storybook deployment test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 import generatedConfig from './tailwind.config.json';
 
-let config = generatedConfig as Config;
+let config: Config = generatedConfig as Config;
 
 if (process.env.ENVIRONMENT_NAME === 'local') config = require('./tailwind.config.local.json');
 if (process.env.NEXT_PUBLIC_IS_STORYBOOK === 'true') require('./tailwind.config.storybook.json');
