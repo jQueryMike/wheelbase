f73ec0fedd0fb63ba8ce7e942efd2d14362cd10d
Jonathon Chambers <jonathon.chambers@clickdealer.co.uk>
2024-01-26 13:08:09 +0000

    Fontawesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
+import { Suspense } from 'react';
 
 import './globals.css';
 
@@ -7,7 +9,17 @@ const inter = Inter({ subsets: ['latin'] });
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Suspense fallback={null}>
+          <Script
+            src="https://kit.fontawesome.com/3bd8a6c68a.js"
+            strategy="beforeInteractive"
+            crossOrigin="anonymous"
+            async
+          />
+        </Suspense>
+      </body>
     </html>
   );
 }
