e0a33e5e8ae9f47e5bb9d0a77a0574c66efb3220
James Walton <107486863+ClickJWalton@users.noreply.github.com>
2024-05-13 09:17:31 +0100

    CLICKWEB-734: Delete old header

--- app/_components/organisms/HeaderOld/Header.types.ts
+++
@@ -1,9 +0,0 @@
-import { BaseProps } from '@components/types';
-
-export type HeaderClasses<T = string> = {
-  [key in 'root']?: T;
-};
-
-export type HeaderProps = BaseProps<{
-  classes?: HeaderClasses<string>;
-}>;
