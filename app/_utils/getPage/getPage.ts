0d43663cb99bff02a5672190b25528d3421ef683
James Walton <107486863+ClickJWalton@users.noreply.github.com>
2024-05-13 21:29:53 +0100

    MONZA-000: Add new method to contact page

--- app/_utils/getPage/getPage.ts
+++ app/_utils/getPage/getPage.ts
@@ -2,7 +2,7 @@ import { ROOT_URL } from '@utils/constants';
 
 export default async function getPage(slug: string[]) {
   const pagesTags = process.env.ENVIRONMENT_NAME !== 'local' ? [`theme`, `page-${slug.join('-')}`] : [];
-  return fetch(`${ROOT_URL}/${slug.join('/')}`, {
+  return fetch(`${ROOT_URL}/${slug.join('/')}?expand=all`, {
     next: { tags: pagesTags },
   }).then((res) => res.json());
 }
