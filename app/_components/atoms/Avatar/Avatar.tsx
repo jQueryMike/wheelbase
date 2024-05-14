2efb912f842dd34e84a2ba3941eac1a29f3a3580
mike-click <152999394+mike-click@users.noreply.github.com>
2024-05-14 10:44:29 +0100

    CLICKWEB-537: Refactored ImageLink & Avatar

--- app/_components/atoms/Avatar/Avatar.tsx
+++ app/_components/atoms/Avatar/Avatar.tsx
@@ -1,26 +1,25 @@
 // eslint-disable-next-line import/no-cycle
 import { Image } from '@components/atoms';
-import { BaseComponent } from '@components/utils';
+import { BaseComponent, Gravatar } from '@components/utils';
 import { buildClasses } from '@utils/buildClasses';
 
 import avatarClasses from './Avatar.classes';
 import { AvatarProps } from './Avatar.types';
 
-const Avatar = ({ styling, overrides, image1: image }: AvatarProps) => {
+const Avatar = ({ gravatarName, styling, overrides, ...image }: AvatarProps) => {
   const classes = buildClasses(avatarClasses, overrides);
-
   return (
-    <BaseComponent as="div" styling={styling} stylingOptions={{ atomicType: 'atom' }} className={classes?.root}>
-      <Image
+    <BaseComponent className={classes?.root} stylingOptions={{atomicType: 'atom'}} styling={styling}>
+      {image?.src ? <Image
         alt={image?.alt || 'Avatar'}
         loading={image?.loading || 'lazy'}
         width={image?.width || '128'}
         height={image?.height || '128'}
         className={classes?.avatar}
-        src={image?.src || ""}
-        id=""
-        styling={image?.styling || {}}
-      />
+        src={image?.src || ''}
+        id=''
+        styling={{}}
+      /> : <Gravatar reviewerName={gravatarName || 'Anon' } styling={{}} />}
     </BaseComponent>
   );
 };
