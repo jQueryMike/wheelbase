b112fb298f6dc07613be9ff3719c40ff97c651db
James Walton <107486863+ClickJWalton@users.noreply.github.com>
2024-05-15 19:42:51 +0100

    MONZA-000f: Icon test suite

--- app/_components/atoms/Icon/Icon.tsx
+++ app/_components/atoms/Icon/Icon.tsx
@@ -1,13 +1,18 @@
-import { BaseComponent } from '@components/utils/BaseComponent';
-import { buildClasses } from '@utils/buildClasses';
-import iconClasses from './Icon.classes';
-
-import { IconProps } from './Icon.types';
+import iconClasses from "./Icon.classes";
+import { IconProps } from "./Icon.types";
+import { BaseComponent } from "@components/utils/BaseComponent";
+import { buildClasses } from "@utils/buildClasses";
 
 const Icon = ({ icon, styling, overrides }: IconProps) => {
   const classes = buildClasses(iconClasses, overrides);
   return (
-    <BaseComponent className={classes.root} as="div" styling={styling} stylingOptions={{ atomicType: 'atom', textType: "icon" }}>
+    <BaseComponent
+      data-testid="icon"
+      className={classes.root}
+      as="div"
+      styling={styling}
+      stylingOptions={{ atomicType: "atom", textType: "icon" }}
+    >
       <i className={`${icon}`} />
     </BaseComponent>
   );
