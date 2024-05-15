311c3d08351330bbad789a44e6fda3bacbdba75e
James Walton <107486863+ClickJWalton@users.noreply.github.com>
2024-05-15 13:19:08 +0100

    CLICKWEB-741: add handling for lack of footer

--- app/_utils/getFooter/getFooter.ts
+++ app/_utils/getFooter/getFooter.ts
@@ -3,14 +3,18 @@ import { buildConfig } from '@utils/buildConfig';
 const getFooter = (globalConfig: any) => {
   if (globalConfig?.properties) {
     const {
-      properties: {
-        footer: { items: footerItems } = { items: '' },
-        companyInfoItems: { items: companyInfoItems } = { items: '' },
-      },
+      properties: { footer, companyInfoItems },
     } = globalConfig || {};
 
-    const [defaultFooter] = [footerItems?.[0]?.content]?.map((x: any) => buildConfig(x)) || [null];
-    const [companyInfo] = [companyInfoItems?.[0]?.content]?.map((x: any) => buildConfig(x)) || [null];
+    const footerItems = footer?.items;
+    const [defaultFooter] = footerItems
+      ? [footerItems?.[0]?.content]?.map((x: any) => buildConfig(x)) || [null]
+      : [null];
+
+    const companyInfoItemsItems = companyInfoItems?.items;
+    const [companyInfo] = companyInfoItemsItems
+      ? [companyInfoItemsItems?.[0]?.content]?.map((x: any) => buildConfig(x)) || [null]
+      : [null];
 
     return { defaultFooter, companyInfo };
   }
