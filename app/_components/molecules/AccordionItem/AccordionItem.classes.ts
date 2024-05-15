aa65e4770c2156d2d5785c7e045f0e37c804969b
James Walton <107486863+ClickJWalton@users.noreply.github.com>
2024-05-15 11:59:07 +0100

    CLICKWEB-792: Style helps with padding on content area

--- app/_components/molecules/AccordionItem/AccordionItem.classes.ts
+++ app/_components/molecules/AccordionItem/AccordionItem.classes.ts
@@ -6,7 +6,7 @@ const location = 'AccordionItem/AccordionItem.classes';
 
 const classes: AccordionItemClasses = {
   root: tw`mx-auto `,
-  accordionHeading: tw`flex cursor-pointer items-center justify-between`,
+  accordionHeading: tw`flex cursor-pointer items-center justify-between p-4`,
 };
 
 const accordionItemClasses = new ClassesBuilder({ location, classes }).classes;
