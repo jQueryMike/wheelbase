1ca6af76db19c2d2dd371b3bb64a4aa93c5a5c46
mike-click <152999394+mike-click@users.noreply.github.com>
2024-05-22 13:45:37 +0100

    CLICKWEB-000: removed line-height from tailwind font-sizes

--- tailwind.builder.mjs
+++ tailwind.builder.mjs
@@ -34,6 +34,16 @@ const generateTailwindConfig = async () => {
           typography: buildTypography(colors),
           colors,
         },
+        fontSize: {
+          'xs': ['0.75rem', {}],
+          'sm': ['0.875rem', {}],
+          'base': ['1rem', {}],
+          'lg': ['1.125rem', {}],
+          'xl': ['1.25rem', {}],
+          '2xl': ['1.5rem', {}],
+          '3xl': ['1.875rem', {}],
+          '4xl': ['2.25rem', {}],
+        }
       },
     };
 
