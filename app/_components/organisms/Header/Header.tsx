e799712e61a6e02b488e72fe0a5645ed7f820106
James Walton <107486863+ClickJWalton@users.noreply.github.com>
2024-05-29 10:14:06 +0100

    MONZA-000: Fix build issues

--- app/_components/organisms/Header/Header.tsx
+++ app/_components/organisms/Header/Header.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 import BLOCKS from '@components/Blocks';
 import { Image, NavigationItem } from '@components/atoms';
 import { DrawerNavigation } from '@components/atoms/DrawerNavigation';
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,25 @@
-import type { Config } from 'tailwindcss';
-import localConfig from './tailwind.config.local.json'
-import stortbookConfig from './tailwind.config.storybook.json';
-import productionConfig from './tailwind.config.json'
+// import type { Config } from 'tailwindcss';
+// import localConfig from './tailwind.config.local.json'
+// import stortbookConfig from './tailwind.config.storybook.json';
+// import productionConfig from './tailwind.config.json'
 
-let config: Config;
+// let config: Config;
 
-switch (process.env.ENVIRONMENT_NAME) {
-  case 'local':
-    config = localConfig;
-    break;
-  case 'storybook':
-    config = stortbookConfig;
-    break;
-  default:
-    config = productionConfig;
-    break;
-}
+// switch (process.env.ENVIRONMENT_NAME) {
+//   case 'local':
+//     config = localConfig;
+//     break;
+//   case 'storybook':
+//     config = stortbookConfig;
+//     break;
+//   default:
+//     config = productionConfig;
+//     break;
+// }
 
-const tailwindConfig: Config = {
-  ...config,
-  plugins: [require('@tailwindcss/container-queries'), require('@tailwindcss/typography')],
-};
+// const tailwindConfig: Config = {
+//   ...config,
+//   plugins: [require('@tailwindcss/container-queries'), require('@tailwindcss/typography')],
+// };
 
-export default tailwindConfig;
+// export default tailwindConfig;
