3142f89044079ccdfce55878386c68fea66427fc
Toby Loudon <toby.loudon@clickdealer.co.uk>
2024-05-30 09:31:39 +0100

    CLICKWEB-827: component structure and styling

--- app/_components/organisms/ImageBlock/ImageBlock.tsx
+++ app/_components/organisms/ImageBlock/ImageBlock.tsx
@@ -1,29 +1,40 @@
-import { BaseComponent } from '@components/utils';
-import { buildClasses } from '@utils/buildClasses';
-import { ImageBlockProps } from './ImageBlock.types';
 import imageBlockClasses from './ImageBlock.classes';
+import { ImageBlockProps } from './ImageBlock.types';
+import { BaseComponent } from '@components/utils';
 import { Block } from '@types';
-import { Image } from '@components/atoms';
+import { buildClasses } from '@utils/buildClasses';
+import NextImage from 'next/image';
 
-const ImageBlock = ({ imageBlock: image, styling, overrides }: ImageBlockProps & Block) => {
+const ImageBlock = ({
+  imageLink: image,
+  styling,
+  overrides,
+}: ImageBlockProps & Block) => {
+  if (!image) {
+    return;
+  }
+
   const classes = buildClasses(imageBlockClasses, overrides);
+
   return (
-    <BaseComponent as="div" className={classes.root} styling={styling} stylingOptions={{ atomicType: 'organism' }}>
-      <section className="relative overflow-hidden w-full max-h-[360px] md:max-h-[420] lg:md:max-h-[500] aspect-[1/1] md:aspect-[3/1] lg:aspect-[4/1] lg:max-h-[500px]">
-        <div className="absolute inset-0">
-            {/* <Image src={'/'} alt="" width="0" height="0" decoding="async" data-nimg="1" className="h-full w-full object-cover object-center" sizes="100vw" ></Image> */}
-            
-            
-            {image && (
-                <div
-                className={''}
-                data-testid="image-container"
-                >
-                    <Image className={classes?.image} {...image} />
-                </div>
-          )}
-        </div>
-        </section>
+    <BaseComponent
+      as="div"
+      className={classes?.root}
+      styling={styling}
+      stylingOptions={{ atomicType: 'organism' }}
+    >
+      <div
+        className={classes?.imageContainer}
+        data-testid="fullscreen-image-container"
+      >
+        <NextImage
+          className={classes?.image}
+          {...image}
+          sizes="100vw"
+          width="0"
+          height="0"
+        />
+      </div>
     </BaseComponent>
   );
 };
--- app/_components/organisms/ImageBlock/ImageBlock.types.ts
+++ app/_components/organisms/ImageBlock/ImageBlock.types.ts
@@ -2,7 +2,7 @@ import { BaseProps } from '@components/types';
 import { ImageProps } from '@components/atoms';
 
 export type ImageBlockClasses<T = string> = {
-  [key in 'root']?: T;
+  [key in 'imageContainer' | 'image' | 'root']?: T;
 }
 
 export type ImageBlockProps = BaseProps<{
