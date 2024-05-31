e9d3d575989b2e846dc5b61e31d37d4c8e5664f9
Toby Loudon <toby.loudon@clickdealer.co.uk>
2024-05-31 10:55:21 +0100

    MONZA-000f: Review Detail Tests

--- app/_components/atoms/ReviewDetail/ReviewDetail.test.tsx
+++ app/_components/atoms/ReviewDetail/ReviewDetail.test.tsx
@@ -1,15 +1,35 @@
-import { render } from '@testing-library/react';
-import { axe } from 'jest-axe';
-
 import ReviewDetail from './ReviewDetail';
+import { ReviewDetailProps } from './ReviewDetail.types';
+import { render, screen, within } from '@testing-library/react';
+import { axe } from 'jest-axe';
+import { act } from 'react-dom/test-utils';
+
+const reviewDetail = 'Sample test text!';
+
+const cases: [string, ReviewDetailProps, () => void][] = [
+  [
+    'Render Review Detail with Text',
+    {
+      reviewDetail: reviewDetail,
+      styling: {},
+    },
+    async () => {
+      const { getByText } = within(await screen.findByTestId('review-detail'));
+      expect(getByText(reviewDetail)).toBeInTheDocument();
+    },
+  ],
+];
 
 describe('ReviewDetail test suite', () => {
-  it('should work', () => {
-    expect(true).toBe(true);
+  it.each(cases)('%s', (_, properties, assertions) => {
+    render(<ReviewDetail {...properties} />);
+    assertions();
   });
 
   it('should have no accessibility violations', async () => {
-    const { container } = render(<ReviewDetail title="Test" styling={{}} />);
+    const { container } = render(
+      <ReviewDetail reviewDetail={reviewDetail} styling={{}} />
+    );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
--- app/_components/atoms/ReviewDetail/ReviewDetail.tsx
+++ app/_components/atoms/ReviewDetail/ReviewDetail.tsx
@@ -1,13 +1,22 @@
+import reviewDetailClasses from './ReviewDetail.classes';
+import { ReviewDetailProps } from './ReviewDetail.types';
 import { BaseComponent } from '@components/utils';
 import { buildClasses } from '@utils/buildClasses';
 
-import reviewDetailClasses from './ReviewDetail.classes';
-import { ReviewDetailProps } from './ReviewDetail.types';
-
-const ReviewDetail = ({ reviewDetail, styling, overrides }: ReviewDetailProps) => {
+const ReviewDetail = ({
+  reviewDetail,
+  styling,
+  overrides,
+}: ReviewDetailProps) => {
   const classes = buildClasses(reviewDetailClasses, overrides);
   return (
-    <BaseComponent as="cite" styling={styling} className={classes.root} stylingOptions={{ atomicType: 'atom' }}>
+    <BaseComponent
+      as="cite"
+      styling={styling}
+      className={classes.root}
+      stylingOptions={{ atomicType: 'atom' }}
+      data-testid="review-detail"
+    >
       {reviewDetail}
     </BaseComponent>
   );
