5b02b4e376067b9809a43396910be3aa105e78b4
James Walton <107486863+ClickJWalton@users.noreply.github.com>
2024-06-03 10:04:40 +0100

    MONZA-000: Avatar test fix

--- app/_components/atoms/Avatar/Avatar.test.tsx
+++ app/_components/atoms/Avatar/Avatar.test.tsx
@@ -1,13 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import { axe } from 'jest-axe';
-import { act } from 'react-dom/test-utils';
-
-import Avatar from './Avatar';
-import { AvatarProps } from './Avatar.types';
+import Avatar from "./Avatar";
+import { AvatarProps } from "./Avatar.types";
+import { render, screen } from "@testing-library/react";
+import { axe } from "jest-axe";
+import { act } from "react-dom/test-utils";
 
 const testAvatar = {
-  src: 'https://fastly.picsum.photos/id/851/200/300.jpg?hmac=AD_d7PsSrqI2zi-ubHY_-urUxCN77Gnev3k5o0P6nlE',
-  alt: 'User Avatar',
+  src: "https://fastly.picsum.photos/id/851/200/300.jpg?hmac=AD_d7PsSrqI2zi-ubHY_-urUxCN77Gnev3k5o0P6nlE",
+  alt: "User Avatar",
   width: 64,
   height: 64,
   styling: {},
@@ -15,45 +14,53 @@ const testAvatar = {
 
 const cases: [string, AvatarProps, () => void][] = [
   [
-    'render Avatar with image',
+    "render Avatar with image",
     {
       ...testAvatar,
     },
     async () => {
-      expect(await screen.findByTestId('avatar')).toBeTruthy();
-      const avatarElement = await screen.findByTestId('avatar-image');
-      expect(avatarElement).toHaveAttribute('src', testAvatar.src);
-      expect(avatarElement).toHaveAttribute('alt', testAvatar.alt);
-      expect(avatarElement).toHaveAttribute('width', `${testAvatar.width}`);
-      expect(avatarElement).toHaveAttribute('height', `${testAvatar.height}`);
+      const avatarElement = await screen.findByTestId("avatar-image");
+      expect(avatarElement).toHaveAttribute("alt", testAvatar.alt);
+      expect(avatarElement).toHaveAttribute("width", `${testAvatar.width}`);
+      expect(avatarElement).toHaveAttribute("height", `${testAvatar.height}`);
+      expect(avatarElement).toHaveAttribute(
+        "src",
+        expect.stringContaining(
+          "/_next/image?url=https%3A%2F%2Ffastly.picsum.photos%2Fid%2F851%2F200%2F300.jpg"
+        )
+      );
     },
   ],
   [
-    'render Avatar with default image values',
+    "render Avatar with default image values",
     {
       src: testAvatar.src,
       alt: testAvatar.alt,
       styling: {},
     },
     async () => {
-      expect(await screen.findByTestId('avatar')).toBeTruthy();
-      const avatarElement = await screen.findByTestId('avatar-image');
-      expect(avatarElement).toHaveAttribute('src', testAvatar.src);
-      expect(avatarElement).toHaveAttribute('alt', 'Avatar');
-      expect(avatarElement).toHaveAttribute('width', '128');
-      expect(avatarElement).toHaveAttribute('height', '128');
-      expect(avatarElement).toHaveAttribute('loading', 'lazy');
+      const avatarElement = await screen.findByTestId("avatar-image");
+      expect(avatarElement).toHaveAttribute("alt", testAvatar.alt);
+      expect(avatarElement).toHaveAttribute("width", "128");
+      expect(avatarElement).toHaveAttribute("height", "128");
+      expect(avatarElement).toHaveAttribute("loading", "lazy");
+      expect(avatarElement).toHaveAttribute(
+        "src",
+        expect.stringContaining(
+          "/_next/image?url=https%3A%2F%2Ffastly.picsum.photos%2Fid%2F851%2F200%2F300.jpg"
+        )
+      );
     },
   ],
 ];
 
-describe('Avatar test suite', () => {
-  it.each(cases)('%s', (_, properties, assertions) => {
+describe("Avatar test suite", () => {
+  it.each(cases)("%s", async (_, properties, assertions) => {
     render(<Avatar {...properties} />);
-    assertions();
+    await assertions();
   });
 
-  it('should have no accessibility violations', async () => {
+  it("should have no accessibility violations", async () => {
     await act(async () => {
       const { container } = render(<Avatar {...testAvatar} />);
       const results = await axe(container);
