de2188d38b270d0c3d76c9c1b512f1707730f8c4
James Walton <107486863+ClickJWalton@users.noreply.github.com>
2024-06-03 10:34:03 +0100

    MONZA-000: Data test id fix

--- app/_components/utils/BaseComponent/BaseComponent.tsx
+++ app/_components/utils/BaseComponent/BaseComponent.tsx
@@ -1,22 +1,26 @@
-import { buildStyling } from '@utils/buildStyling';
-import cn from 'classnames';
-import { PropsWithChildren } from 'react';
-
-import { BaseComponentProps } from './BaseComponent.types';
+import { BaseComponentProps } from "./BaseComponent.types";
+import { buildStyling } from "@utils/buildStyling";
+import cn from "classnames";
+import { PropsWithChildren } from "react";
 
 const BaseComponent = <T extends keyof HTMLElementTagNameMap>({
   as,
   className,
   styling,
-  stylingOptions = { atomicType: 'organism' },
+  stylingOptions = { atomicType: "organism" },
   children,
+  datatestid,
 }: PropsWithChildren<BaseComponentProps<T>>) => {
-  const Component = as || 'section';
+  const Component = as || "section";
   const [baseClasses, baseVars] = buildStyling(styling, stylingOptions);
 
   return (
     // @ts-ignore - TS doesn't like the `as` prop as too generic
-    <Component className={cn(className, baseClasses)} style={baseVars}>
+    <Component
+      className={cn(className, baseClasses)}
+      style={baseVars}
+      data-testid={datatestid}
+    >
       {children}
     </Component>
   );
--- app/_components/utils/BaseComponent/BaseComponent.types.ts
+++ app/_components/utils/BaseComponent/BaseComponent.types.ts
@@ -1,9 +1,11 @@
-import { BaseProps } from '@components/types';
+import { BaseProps } from "@components/types";
 
-export type BaseComponentProps<T extends keyof HTMLElementTagNameMap> = BaseProps<{
-  overrides?: any; // TODO: Define method for overrides
-  className?: string;
-}> & {
-  as?: T;
-  stylingOptions?: any;
-} & React.HTMLProps<HTMLElementTagNameMap[T]>;
+export type BaseComponentProps<T extends keyof HTMLElementTagNameMap> =
+  BaseProps<{
+    overrides?: any; // TODO: Define method for overrides
+    className?: string;
+  }> & {
+    as?: T;
+    stylingOptions?: any;
+    datatestid?: string;
+  } & React.HTMLProps<HTMLElementTagNameMap[T]>;
