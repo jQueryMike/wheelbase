7dbbbbbfb02f7b54eef85f3e7ec0fa5193ae48aa
Toby Loudon <toby.loudon@clickdealer.co.uk>
2024-06-04 14:32:46 +0100

    CLICKWEB-827: link accounted for in image block component

--- app/_components/organisms/ImageBlock/ImageBlock.tsx
+++ app/_components/organisms/ImageBlock/ImageBlock.tsx
@@ -2,16 +2,30 @@ import imageBlockClasses from './ImageBlock.classes';
 import { ImageBlockProps } from './ImageBlock.types';
 import { BaseComponent } from '@components/utils';
 import { Block } from '@types';
+import { buildLink } from '@utils';
 import { buildClasses } from '@utils/buildClasses';
 import NextImage from 'next/image';
+import Link from 'next/link';
 
-const ImageBlock = ({ imageLink: image, styling, overrides }: ImageBlockProps & Block) => {
+const ImageBlock = ({
+  imageLink: image,
+  styling,
+  overrides,
+}: ImageBlockProps & Block) => {
   if (!image) {
     return null;
   }
 
   const classes = buildClasses(imageBlockClasses, overrides);
   const fullscreen = image.fullscreen;
+  const parsedLink = buildLink(image.link);
+
+  const renderImage = (
+    <NextImage
+      className={!fullscreen ? classes.image : classes.imageFullscreen}
+      {...image}
+    />
+  );
 
   return (
     <BaseComponent
@@ -21,10 +35,20 @@ const ImageBlock = ({ imageLink: image, styling, overrides }: ImageBlockProps & 
       stylingOptions={{ atomicType: 'organism' }}
     >
       <div
-        className={!fullscreen ? classes.imageContainer : classes.imageContainerFullscreen}
+        className={
+          !fullscreen
+            ? classes.imageContainer
+            : classes.imageContainerFullscreen
+        }
         data-testid="fullscreen-image-container"
       >
-        <NextImage className={!fullscreen ? classes.image : classes.imageFullscreen} {...image} />
+        {image.link ? (
+          <Link {...parsedLink} data-testid="image-link">
+            {renderImage}
+          </Link>
+        ) : (
+          renderImage
+        )}
       </div>
     </BaseComponent>
   );
