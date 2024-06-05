96bf31a4832512e4b348d14a65e59a4bbfa74023
mike-click <152999394+mike-click@users.noreply.github.com>
2024-06-05 10:36:42 +0100

    CLICKWEB-853: home now renders at / all subpages load as they should

--- next.config.js
+++ next.config.js
@@ -21,6 +21,22 @@ const nextConfig = {
     NEXT_PUBLIC_IS_STORYBOOK: process.env.NEXT_PUBLIC_IS_STORYBOOK,
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
   },
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+      // Wildcard path matching
+      {
+        source: '/home/:slug',
+        destination: '/:slug',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
