2b781a0e17a570b297c8f535bbb82e0a724e09c0
mike-click <152999394+mike-click@users.noreply.github.com>
2024-06-05 13:58:00 +0100

    CLICKWEB-863: fixed imageAsBackground on dom error and socials links by changing back to v1 api endpoint

--- app/_utils/getPage/getPage.ts
+++ app/_utils/getPage/getPage.ts
@@ -1,6 +1,6 @@
-import { ROOT_URL_V2 } from '@utils/constants';
+import { ROOT_URL } from '@utils/constants';
 
 export default async function getPage(slug: string[]) {
   const pagesTags = process.env.ENVIRONMENT_NAME !== 'local' ? [`theme`, `page-${slug.join('-')}`] : [];
-  return fetch(`${ROOT_URL_V2}/${slug.join('/')}`, { next: { tags: pagesTags } }).then((res) => res.json());
+  return fetch(`${ROOT_URL}/${slug.join('/')}?expand=all`, { next: { tags: pagesTags } }).then((res) => res.json());
 }
